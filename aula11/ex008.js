6775b3660e6f7a466de78aafb75909b9fd19764e
Leandro Paranhos <145766259+lparanhoskoda@users.noreply.github.com>
2024-09-21 17:12:34 -0300

    create ex010

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 12
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if(vel > 60){
+    console.log(`Voce ultrapassou a velocidade permmitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
