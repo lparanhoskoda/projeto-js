315e30ef1ffd81fc47e4931bc3022962fd5ec2d0
Leandro Paranhos <145766259+lparanhoskoda@users.noreply.github.com>
2024-09-23 23:00:40 -0300

    att ex014

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,22 @@
-alert('Olá')
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 19
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12){
+        // bom dia
+        img.src = 'imagens/manha.png'
+        document.body.style.background = '#aabdc9'
+    } else if (hora >= 12 && hora <= 18){
+        // boa tarde
+        img.src = 'imagens/tarde.png'
+        document.body.style.background = '#9ab38d'
+    } else {
+        // boa noite
+        img.src = 'imagens/noite.png'
+        document.body.style.background = '#472d43'
+    }
+    
+}
